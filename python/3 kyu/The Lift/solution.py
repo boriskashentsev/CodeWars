660453e195bc3123ef087150090304937739920f
boriskashentsev <boris.kashentsev@gmail.com>
2018-03-27 18:51:21 +0300

    Added pre-solution code

--- python/3 kyu/The Lift/solution.py
+++ python/3 kyu/The Lift/solution.py
@@ -1,1 +1,25 @@
+class Dinglemouse(object):
 
+    def __init__(self, queues, capacity):
+        pass
+        
+    def theLift(self):
+        return []
+
+def testing(value1, value2):
+    if (value1 == value2):
+        print "Yep!"
+    else:
+        print "Nah."
+
+# Floors:    G     1      2        3     4      5      6         Answers:
+tests = [[ ( (),   (),    (5,5,5), (),   (),    (),    () ),     [0, 2, 5, 0]          ],
+         [ ( (),   (),    (1,1),   (),   (),    (),    () ),     [0, 2, 1, 0]          ],
+         [ ( (),   (3,),  (4,),    (),   (5,),  (),    () ),     [0, 1, 2, 3, 4, 5, 0] ],
+         [ ( (),   (0,),  (),      (),   (2,),  (3,),  () ),     [0, 5, 4, 3, 2, 1, 0] ]]
+  
+for queues, answer in tests:
+    lift = Dinglemouse(queues, 5)
+    testing(lift.theLift(), answer)
+    
+    
