560b99224e85200bff028064eb9db31016d46e7d
boriskashentsev <boris.kashentsev@gmail.com>
2018-03-27 18:12:21 +0300

    Removed testing code

--- python/6 kyu/Kebabize/solution.py
+++ python/6 kyu/Kebabize/solution.py
@@ -6,19 +6,7 @@ def kebabize(string):
                 result += "-" + char.lower()
             else:
                 result += char
-    #print result
     if (result != '')and(result[0] == "-"):
         result = result[1:]
     return result
-
-def testing(value1, value2): 
-    if (value1 == value2):
-        print "Yep"
-    else:
-        print "Nope"
-
-testing(kebabize('myCamelCasedString'), 'my-camel-cased-string')
-testing(kebabize('myCamelHas3Humps'), 'my-camel-has-humps')
-testing(kebabize('SOS'), 's-o-s')
-testing(kebabize('42'), '')
-testing(kebabize('CodeWars'), 'code-wars')
+    
