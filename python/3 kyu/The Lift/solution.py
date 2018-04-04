afcce0d76557e84d2e23ffc62ee6634d67d8d4ae
boriskashentsev <boris.kashentsev@gmail.com>
2018-04-04 16:19:29 +0300

    Added method that would look for the list of floors that the lift needs to visit right after switching the direction of the movement

--- python/3 kyu/The Lift/solution.py
+++ python/3 kyu/The Lift/solution.py
@@ -1,10 +1,57 @@
 class Lift(object):
     def __init__(self, capacity):
-        self.direction = 1 # "1" for up and "-1" for down
+        self.direction = -1 # "1" for up and "-1" for down. it is assigned -1 at the beginning because we treat as if Lift just arrived from the top and ready to start it's movement up
         self.capacity = capacity
         self.people = []
         self.floor = 0
         self.destination = -1
+        self.floorsToVisit = []
+
+    # function that seeks floors that Lift needs to visit.
+    # we assume that function is called right before Lift is switching its direction
+    def findFloorsToVisit(self, queues):
+        self.direction = -self.direction
+        self.floorsToVisit = self.people
+        floorsPossibleToVisit = []
+        if (self.direction > 0):
+            floorsPossibleToVisit = range(self.floor, len(queues))
+        else:
+            floorsPossibleToVisit = range(self.floor, -1, -1)
+        for floor in floorsPossibleToVisit:
+            peopleOnIthFloor = queues[floor]
+            if (self.direction > 0): # lift goes up
+                if (len([person for person in peopleOnIthFloor if person > floor]) > 0): # there are people going up from the current floor
+                    self.floorsToVisit.append(floor)
+            if (self.direction < 0): # lift goes down
+                if (len([person for person in peopleOnIthFloor if person < floor]) > 0): # there are people going down from the current floor
+                    self.floorsToVisit.append(floor)
+        self.floorsToVisit = list(set(self.floorsToVisit))
+        self.floorsToVisit.sort()
+        if (self.direction < 0):
+            self.floorsToVisit.reverse()
+
+        # At this point we have a list of floors that asked for the lift that are going in the same direction as the lift.
+        # we are missing floors that are past the maximum (minimum when going down) floor, that are going the opposite direction.
+        if (self.direction > 0) :
+            floorsPossibleToVisit = range(len(queues)-1, self.floor,-1)
+        else :
+            floorsPossibleToVisit = range(0,self.floor)
+        
+        for floor in floorsPossibleToVisit:
+            peopleOnIthFloor = queues[floor]
+            if (self.direction > 0):
+                if (len([person for person in peopleOnIthFloor if person < floor]) > 0):
+                    self.floorsToVisit.append (floor)
+                    break
+            else:
+                if (len([person for person in peopleOnIthFloor if person > floor]) > 0):
+                    self.floorsToVisit.append (floor)
+                    break
+
+        print(self.floorsToVisit)
+
+
+        
 
 class Dinglemouse(object):
 
@@ -14,8 +61,7 @@ class Dinglemouse(object):
         
     def theLift(self):
         result = [0]
-        print queues
-        print self.lift.capacity
+        self.lift.findFloorsToVisit(queues)
         return result
 
 def testing(value1, value2):
