3513fe4d1bd32dab7175772e957627fabc475b15
boriskashentsev <boris.kashentsev@gmail.com>
2018-08-03 22:57:01 +0300

    Added solution

--- python/3 kyu/Huffman Encoding/solution.py
+++ python/3 kyu/Huffman Encoding/solution.py
@@ -1,19 +1,90 @@
-def find_pattern(sequence):
-    return [1]
+# takes: str; returns: [ (str, int) ] (Strings in return value are single characters)
+def frequencies(s):
+    hist = {} # histogram
+    for char in s:
+        if char not in hist:
+            hist[char] = 1
+        else :
+            hist[char] += 1
+    if len(hist.keys()) < 1 :
+        return []
+    # convert to list which will be pro manipulated
+    freqs = []
+    for key in hist.keys() :
+        freqs.append((key, hist[key]))
+    return freqs
 
-def testing(value1, value2):
-    if (value1 == value2):
-        print "Yep!"
-    else:
-        print "Nah."
+def buildATree(freqs) :
+    if freqs == None:
+        return
+    tree = []
+    for element in freqs:
+        tree.append([element[0], element[1]])
+    tree = sorted(tree, key = lambda pair: pair[1], reverse = True) # sort by number of appearances
+    if len(tree) > 1 :
+        while len(tree) > 2 :
+            newNode = [[tree[-2][0], tree[-1][0]], tree[-2][1] + tree[-1][1]]
+            tree.pop()
+            tree.pop()
+            locs = [loc for loc, val in enumerate(tree) if val[1] >= newNode[1]]
+            index = max(locs) if len(locs) > 0 else -1
+            tree.insert(index + 1, newNode)
+        tree = [tree[0][0], tree[1][0]]
+        return tree
+    else :
+        return
 
-tests = [[ [1, 2, 3, 4, 5],                                         [1] ],
-         [ [1, 2, 3, 4, 5, 4, 3, 2, 1, 2, 3, 4, 5, 4, 3, 2, 1],     [1, 1, 1, 1, -1, -1, -1, -1] ],
-         [ [1, 5, 2, 3, 1, 5, 2, 3, 1],                             [4, -3, 1, -2] ],
-         [ [1, 5, 4, 8, 7, 11, 10, 14, 13],                         [4, -1] ],
-         [ [0, 1],                                                  [1] ],
-         ]
+def goDeep(node, prefix, dictionary):
+    if len(node) > 1 :
+        goDeep(node[0], prefix + "0", dictionary)
+        goDeep(node[1], prefix + "1", dictionary)
+    else :
+        dictionary[node] = prefix
+
+# takes: [ (str, int) ], str; returns: String (with "0" and "1")
+def encode(freqs, s):
+    result = ""
+    tree = buildATree(freqs)
+    if tree == None:
+        return
+    charDict = {}
+    goDeep(tree[0], "0", charDict)
+    goDeep(tree[1], "1", charDict)
+    for char in s :
+        result += charDict[char]
+    return result
   
-for indx, [task, answer] in enumerate(tests):
-    print ("Test #%d :" % indx)
-    testing(find_pattern(task), answer)
+# takes [ [str, int] ], str (with "0" and "1"); returns: str
+def decode(freqs,bits):
+    result = ""
+    tree = buildATree(freqs)
+    if tree == None:
+        return
+    ind = 0
+    while ind < len(bits) :
+        node = tree[int(bits[ind])]
+        while len(node) > 1:
+            ind += 1
+            node = node[int(bits[ind])]
+        result += node
+        ind += 1
+    return result
+
+print "Test 1"
+s = "aaaabcc"
+fr = frequencies(s)
+encoded = encode(fr, s)
+print encoded
+decoded = decode(fr, encoded)
+print decoded
+print decoded == s
+
+
+print "Test 2"
+s = "aaaabaaacbbbbccgdfgertebhsfsdvsnfjskdhgfsudhfjwekhbfjvsgvhkjshdjfklshjufgshjdfbhsjkdf"
+fr = frequencies(s)
+encoded = encode(fr, s)
+print encoded
+decoded = decode(fr, encoded)
+print decoded
+print decoded == s
