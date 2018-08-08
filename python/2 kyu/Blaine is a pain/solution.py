f351a70c0efc3651bb771d5cd4726ab1ce650720
boriskashentsev <boris.kashentsev@gmail.com>
2018-08-08 16:54:21 +0300

    Found couple of bugs that prevented calculating the steps on the track correctly. Now solution passes all the tests.

--- python/2 kyu/Blaine is a pain/solution.py
+++ python/2 kyu/Blaine is a pain/solution.py
@@ -8,13 +8,25 @@ class train:
         self.position = trPos
         self.direction = -1 if tr[0].isupper() else 1
         self.letter = tr[0].upper()
+        self.moved = False
     def __repr__(self):
         return "Train: %s Lenth: %d, Position: %d, Direction: %d" %(self.letter, self.length, self.position, self.direction)
     
 
-dirOfMove = [   [ -1, -1,  0,  1,  1,  1,  0, -1],
-                [  0, -1, -1, -1,  0,  1,  1,  1]]
-            #   [  ^, <^,  <, <v,  v, v>,  >, ^>] 
+dirOfMove = [   [ -1 ,  -1 ,  0 ,  1 ,  1 ,   1 ,  0 , -1],
+                [  0 ,  -1 , -1 , -1 ,  0 ,   1 ,  1 ,  1],
+                [ '|', '\\', '-', '/', '|', '\\', '-', '/']]
+            #   [  ^ ,  <^ ,  < , <v ,  v ,  v> ,  > , ^>]
+
+possibleMoves= {
+    '-': [0],
+    '|': [0],
+    'S': [0],
+    'X': [0],
+    '+': [0],
+    '/': [0, -1, 1],
+    '\\':[0, -1, 1]
+}
 
 def moveDirection(ind) :
     return [dirOfMove[0][ind], dirOfMove[1][ind]]
@@ -42,58 +54,48 @@ def trackToSteps(track):
             break
     curDir = 7 # curDirection -> range(8)
     curLoc = start
+    curStatus = modTrack[start[0]][start[1]]
+    trueCurStatus = track[start[0]][start[1]]
     curStep = 0
     while curLoc != start or curStep == 0 :
-        for ind in [0, -1, -2, 1, 2] :
+        for ind in possibleMoves[trueCurStatus] :
             newLoc = newLocation(curLoc, moveDirection((curDir + ind) % 8))
             if isInsideMap(curLoc, moveDirection((curDir + ind) % 8), [len(modTrack), len(modTrack[newLoc[0]]) if len(modTrack) > newLoc[0] else 0]):
                 mapStatus = whatOnMap(modTrack, curLoc, moveDirection((curDir + ind) % 8))
-                if type(mapStatus) == str and mapStatus in "-|SX+/\\" or type(mapStatus) == list: # There should be other statuses. Make changes
+                trueMapStatus = whatOnMap(track, curLoc, moveDirection((curDir + ind) % 8))
+                expectedStatus = dirOfMove[2][(curDir + ind) %8] + '+'
+                if trueCurStatus in '/\\' and not ((trueMapStatus in expectedStatus and ind != 0) or (trueMapStatus in '\\/XS' and ind == 0)):
+                        pass
+                elif type(mapStatus) == str and mapStatus in "-|SX+/\\" or type(mapStatus) == list: # There should be other statuses. Make changes
                     if type(modTrack[curLoc[0]][curLoc[1]]) == str :
                         modTrack[curLoc[0]][curLoc[1]] = [curStep]
                     elif type(modTrack[curLoc[0]][curLoc[1]]) == list :
                         modTrack[curLoc[0]][curLoc[1]].append(curStep)
-                    else :
-                        # print "Weird modified track status. Step:", curStep, "Location:", newLocation(curLoc, moveDirection(curDir))
-                        # exit()
+                    else : # error on the map
                         pass
                     steps.append(curLoc)
-                    curDir = (curDir + ind) % 8
-                    curLoc = newLocation(curLoc, moveDirection(curDir))
+                    curLoc = newLocation(curLoc, moveDirection((curDir + ind) % 8))
+                    if trueMapStatus in '/\\' and trueCurStatus not in '/\\':
+                        for i in [-1, 1]:
+                            if dirOfMove[2][(curDir + i) % 8] == mapStatus:
+                                curDir = (curDir + i) % 8
+                                break
+                    else :
+                        curDir = (curDir + ind) % 8
                     curStep += 1
+                    curStatus = mapStatus
+                    trueCurStatus = track[curLoc[0]][curLoc[1]]
                     break
                 elif mapStatus == ' ':
                     pass
-                else :
-                    # print "Weird map status. Step:", curStep, "Location:", newLocation(curLoc, moveDirection((curDir + ind) % 8))
-                    # exit()
+                else : # error on the map
                     pass
     return (steps, modTrack)
-
-def printTrack(steps,track): # only printing
-    for row in track:
-        line = ''
-        for element in row:
-            if element == ' ':
-                line += ' '
-            else:
-                char = ''
-                for stepNum in element:
-                    if steps[stepNum][1] != '':
-                        if char == '':
-                            char = steps[stepNum][1]
-                            break
-                if char == '':
-                    line += '.'
-                else :
-                    line += char
-        print line
-    print ''
 
 # testing return emptyTrak, otherwise boolean would suffice
 def putTrains(steps, track, trains):
     emptyTrack = list(map(lambda x: [x, ''], steps))
-    for ind,train in enumerate(trains):
+    for train in trains:
         pos = train.position
         location = steps[pos]
         poss = track[location[0]][location[1]] # possible positions to check
@@ -108,23 +110,38 @@ def putTrains(steps, track, trains):
                 if emptyTrack[pos][1] == '' and emptyTrack[otherPos][1] =='' :
                     emptyTrack[pos][1] = train.letter.lower()
                 else :
-                    # if ind > 0:
-                    #     print "Trains intersected"
-                    # else:
-                    #     print "Train intersected itself"
-                    return {}
+                    return []
         else :
-            # print "Trains intersected"
-            return {}
+            return []
     return emptyTrack
+
+# Similar to putTrain but we manipulate only two elements
+def moveTrainsOnTracks(steps, track, trains):
+    newSteps = steps[:]
+    for train in trains:
+        if train.moved:
+            newSteps[(train.position - train.direction * (train.length))%len(steps)][1] = ''
+    for train in trains:
+        if train.moved:
+            pos = train.position
+            location = steps[pos][0]
+            poss = track[location[0]][location[1]] # possible positions to check
+            otherPos = pos if len(poss) == 1 else poss[0] if poss[0] != pos else poss[1]
+            if newSteps[pos][1] == '' and newSteps[otherPos][1] == '' :
+                newSteps[(pos - train.direction)%len(steps)][1] = train.letter.lower()
+                newSteps[pos][1] = train.letter
+            else :
+                return []
+    return newSteps
 
 def moveTrains(steps, track, trains):
     for train in trains: # moving trains here and then putting them to the track
         if train.onStation > 0:
             train.onStation -= 1
-            # print "%s train on station" % (train.letter)
+            train.moved = False
         else:
             train.position = (train.position + train.direction) % len(steps)
+            train.moved = True
             if not train.isExpress :
                 loc = steps[train.position]
                 if track[loc[0]][loc[1]] == 'S':
@@ -141,12 +158,11 @@ def train_crash(track, a, aPos, b, bPos, limit):
     if len(stepsWTrains) == 0:
         return time
     while time <= limit:
-        stepsWTrains = putTrains(steps, nTrack, trains)
+        if time > 0:
+            stepsWTrains = moveTrainsOnTracks(stepsWTrains, nTrack, trains)
         if len(stepsWTrains) == 0:
             return time
-        # if time in range(510,520): # for bigger test to check the moment of the intersection
-        #     print "---> Time: %d <---" % (time)
-        #     printTrack(stepsWTrains, nTrack)
+        #printTrack(stepsWTrains, nTrack)
         moveTrains(steps, track.split('\n'), trains)
         time += 1
     return -1
@@ -155,9 +171,9 @@ def train_crash(track, a, aPos, b, bPos, limit):
 
 def testing(value1, value2):
     if (value1 == value2):
-        print "Yep!"
+        print "Correct!"
     else:
-        print "Nah."
+        print "Nope. Expected %s, received %s" % (str(value2), str(value1))
 
 TRACK_EX = """\
 /---\\
@@ -166,7 +182,31 @@ TRACK_EX = """\
 |   |
 \\-S-/
 """
-testing(train_crash(TRACK_EX, "aaaaaA", 4, "Bbb", 11, 1000), 7)
+testing(train_crash(TRACK_EX, "aaaaaA", 4, "Bbb", 11, 100), 7)
+
+TRACK_EX = """\
+   /---\\
+   |   |
+   |   |
+/--//--/
+|   |
+|   |
+\\---/
+"""
+testing(train_crash(TRACK_EX, "aaaA", 4, "bbB", 17, 100), -1)
+
+TRACK_EX = """\
+/-------\\ 
+|       | 
+S       | 
+|       | 
+\\-------+--------\\
+        |        |
+        S        |
+        |        |
+        \\--------/
+# """
+testing(train_crash(TRACK_EX, "Aaaaaaa", 14, "Xxxxxxxxxx", 3, 2000), 65)
 
 TRACK_EX = """\
                                 /------------\\
